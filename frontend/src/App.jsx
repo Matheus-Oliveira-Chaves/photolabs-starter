eaaa7edf47deafdf9457775191fc4d2f23367b5c
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-12-29 22:04:34 -0500

    ADD photoListItem and styli it, work in app.jsx to display 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
-const App = () => {
-  return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
-  );
-};
+
+const App = () => (
+  <div className="App">
+    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <PhotoList/>
+  </div>
+)
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,27 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+
   /* Insert React */
+
+
+return (
+  <div className="photo-list__item">
+  <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image" />
+
+  <div className="photo-list__user-details">
+    <img src={profile} alt={`Profile ${username}`} className="photo-list__user-profile" />
+    
+    <div className="photo-list__user-info">
+      <p className="username">{username}</p>
+      <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
+    </div>
+  </div>
+</div>
+);
 };
 
 export default PhotoListItem;
