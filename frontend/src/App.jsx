a4d995ac5c1b3ce1f0798edf93f759af2fb9faee
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-12-30 21:09:28 -0500

    ADD homeroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TopNavigationBar from './components/TopNavigationBar';
 
 import PhotoList from 'components/PhotoList';
 import './App.scss';
@@ -6,8 +7,11 @@ import './App.scss';
 
 const App = () => (
   <div className="App">
+    <div className="home-route">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <TopNavigationBar />
     <PhotoList/>
+    </div>
   </div>
 )
 
